247ac0901c3116075f40f9efb733ae6ad4a6dfbe
htavakkoli1368 <hosseinjealous@gmail.com>
2023-08-27 14:19:26 +0330

    final test

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,7 +1,7 @@
 // https://on.cypress.io/api
 
 describe('My First Test', () => {
-  it('visits the app root url', () => {
+  it('visits the app root url by hossein', () => {
     cy.visit('/')
     cy.contains('h1', 'You did it!')
   })
