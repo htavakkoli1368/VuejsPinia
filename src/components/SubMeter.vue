4373b82e67edbae58cae1820146ad9cd486ff357
htavakkoli1368 <hosseinjealous@gmail.com>
2023-08-28 12:46:37 +0330

    remove forn

--- src/components/SubMeter.vue
+++ src/components/SubMeter.vue
@@ -3,26 +3,6 @@
     <p>our props:</p>
     <h6>{{ data }}</h6>
     <button @click="btnClick">click me</button>
-    <div style="width: 400px; height: 130px; margin-top: 20px; border-style: dotted">
-      <br />
-      <span>Firstmame: {{ firstname }}</span> <br />
-      <span>Age: {{ age }}</span> <br />
-    </div>
-    <br /><br />
-    <label> Enter Firstname </label><br />
-    <input
-      type="text"
-      v-model="search"
-      style="font-size: 20px; border-radius: 10px"
-      placeholder=" Name ..."
-    />
-    <br />
-    <br />
-    <button type="button" @click="getAge">Guess Age</button>
-    <br />
-    <br />
-    <br />
-    <input type="radio" value="pop" /> <label>Save my data</label>
   </div>
 </template>
 
